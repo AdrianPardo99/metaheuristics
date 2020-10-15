03f6d981fd33166eda9dd21f3fb2dcf515752472
Adrian González Pardo <gozapaadr@gmail.com>
2020-10-14 22:21:53 -0500

    Correction activity

--- actividadP1_2/rmhc.c
+++ actividadP1_2/rmhc.c
@@ -1,9 +1,17 @@
 
 1.- best_evaluated=random(String) /* Selecciona una cadena aleatoria */
+/* Ejemplo una cadena de bits
+    5 productos ()
+    10100 -> Verificar que sea valia con respecto al peso maximo
+  1.1 Calcular f_best <- Calculo de beneficio
+
+
+*/
 i=0
 3.-while(i<number_evaluated_lim){ /* Vuelve a paso 2 */
-  2.- locus=random(best_evaluated) /* Mutacion random para locus que va de 0 a best_evaluated-1 */
-  new_evaluated=mutation(best_evaluated,locus)
+  2.- locus=random(best_evaluated) /* Mutacion random para locus que va de 0 a best_evaluated-1 escoger 1 bit */
+  new_evaluated=mutation(best_evaluated,locus) /* si x cambia a ~x*/
+  /* Verifico si new_evaluated es valido */
   if fitness(best_evaluated)<=fitness(new_evaluated)
     best_evaluated=new_evaluated
   i++
