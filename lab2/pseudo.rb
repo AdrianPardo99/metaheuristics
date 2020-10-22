0d15163c0d02b15674b992eb396431789b2434cb
Adrian González Pardo <gozapaadr@gmail.com>
2020-10-22 11:36:35 -0500

    Lab report practice 2

--- lab2/pseudo.rb
+++ lab2/pseudo.rb
@@ -3,10 +3,11 @@ s<-Solucion_inicial
 e_old<-funcion_fitness(s)
 t<-temp_max
 while t< temp_min
-  # temp_min es aproximado a 1
+  # temp_min es aproximado a 0
   i=0
   while i<imax
     # imax puede ser representativo dependiendo del problema que se aborda
+    #  o en todo caso puede ser el numero de iteraciones maxima
     seleccion_solucion_sucesor_de(s)
     # seleccion_solucion_sucesor_de es una funcion que explora la vecindad del arbol
     #    elige aleatoriamente
