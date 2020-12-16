6f5313e3c3d474793edd45b5592c91e65bc4d6f0
Adrian González Pardo <gozapaadr@gmail.com>
2020-12-15 21:06:28 -0600

    Function optimization GA Stacionary code

--- functionGA/ga.rb
+++ functionGA/ga.rb
@@ -35,53 +35,55 @@ class GA
       #  "#{sprintf("%.2f",arr_eval[-1])}"
     }
     while iter<@max_iteration
-      s=Seleccion.new(arr_eval)
-      p1=s.torneo
-      p2=s.torneo
-      #puts "\tPadre 1: #{p1}\tPadre 2: #{p2}"
-      c=Crossover.new(arr_sol[p1],arr_sol[p2])
-      c.aritmetic
-      c1=c.child1
-      c2=c.child2
-      #puts "\t\tCruza aritmetica\n\tHijo 1: #{@p.get_coord(c1)}\t"+
-      #  "Hijo 2: #{@p.get_coord(c2)}"
-      m=Mutation.new(c1)
-      m.mut_real(-10.0,10.0,0.05)
-      c1=m.array
-      m.array=c2
-      m.mut_real(-10.0,10.0,0.05)
-      c2=m.array
-      #puts "\t\tMutacion aritmetica\n\tHM1: #{@p.get_coord(c1)}\t"+
-      # "HM2: #{@p.get_coord(c2)}"
-      ec1=@p.get_eval(c1)
-      ec2=@p.get_eval(c2)
-      r=Replacement.new([@p.get_eval(arr_sol[p1])],[@p.get_eval(arr_sol[p2])],
-        [ec1],[ec2])
-      # Opcion de Minimizacion
-      string=r.elitism(0)
-      if !string.include?(:p1)||!string.include?(:p2)
-        if !string.include?:p1
-          if string.include?:p2
-            arr_sol[p1]=arr_sol[p2]
-            arr_eval[p1]=arr_eval[p2]
-          elsif string.include?(:c1)
-            arr_sol[p1]=c1
-            arr_eval[p1]=ec1
-          elsif string.include?(:c2)
-            arr_sol[p1]=c2
-            arr_eval[p1]=ec2
+      if rand()<0.5
+        s=Seleccion.new(arr_eval)
+        p1=s.torneo
+        p2=s.torneo
+        #puts "\tPadre 1: #{p1}\tPadre 2: #{p2}"
+        c=Crossover.new(arr_sol[p1],arr_sol[p2])
+        c.aritmetic
+        c1=c.child1
+        c2=c.child2
+        #puts "\t\tCruza aritmetica\n\tHijo 1: #{@p.get_coord(c1)}\t"+
+        #  "Hijo 2: #{@p.get_coord(c2)}"
+        m=Mutation.new(c1)
+        m.mut_real(-10.0,10.0,0.05)
+        c1=m.array
+        m.array=c2
+        m.mut_real(-10.0,10.0,0.05)
+        c2=m.array
+        #puts "\t\tMutacion aritmetica\n\tHM1: #{@p.get_coord(c1)}\t"+
+        # "HM2: #{@p.get_coord(c2)}"
+        ec1=@p.get_eval(c1)
+        ec2=@p.get_eval(c2)
+        r=Replacement.new([@p.get_eval(arr_sol[p1])],[@p.get_eval(arr_sol[p2])],
+          [ec1],[ec2])
+        # Opcion de Minimizacion
+        string=r.elitism(0)
+        if !string.include?(:p1)||!string.include?(:p2)
+          if !string.include?:p1
+            if string.include?:p2
+              arr_sol[p1]=arr_sol[p2]
+              arr_eval[p1]=arr_eval[p2]
+            elsif string.include?(:c1)
+              arr_sol[p1]=c1
+              arr_eval[p1]=ec1
+            elsif string.include?(:c2)
+              arr_sol[p1]=c2
+              arr_eval[p1]=ec2
+            end
           end
-        end
-        if !string.include?:p2
-          if string.include?:p1
-            arr_sol[p2]=arr_sol[p1]
-            arr_eval[p2]=arr_eval[p1]
-          elsif string.include?(:c1)
-            arr_sol[p2]=c1
-            arr_eval[p2]=ec1
-          elsif string.include?(:c2)
-            arr_sol[p2]=c2
-            arr_eval[p2]=ec2
+          if !string.include?:p2
+            if string.include?:p1
+              arr_sol[p2]=arr_sol[p1]
+              arr_eval[p2]=arr_eval[p1]
+            elsif string.include?(:c1)
+              arr_sol[p2]=c1
+              arr_eval[p2]=ec1
+            elsif string.include?(:c2)
+              arr_sol[p2]=c2
+              arr_eval[p2]=ec2
+            end
           end
         end
       end
